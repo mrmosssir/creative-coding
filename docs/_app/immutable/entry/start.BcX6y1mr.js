68c2ff21908be70eae865e1b8b2a4342cfa7043c
SuTo <mrmosssir@gmail.com>
2025-10-29 14:05:15 +0800

    [build] move output to docs folder

---
+++ docs/_app/immutable/entry/start.BcX6y1mr.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/CSTT9bdW.js";export{o as load_css,r as start};
--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,13 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'docs',
+			assets: 'docs',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
 	}
 };
 
